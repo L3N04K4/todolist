fa355818eeeb84cfadf6a8fafd5727b7db5c9a47
Lena <l3n4.iakusheva@yandex.ru>
2024-06-22 19:43:47 +0300

    fixtures

--- base/models.py
+++ base/models.py
@@ -3,37 +3,51 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from simple_history.models import HistoricalRecords
 
+
 class Categories(models.Model):
     name = models.CharField(max_length=200, db_index=True)
+
     def __str__(self):
         return self.name
 
+
 class Hashtag(models.Model):
     name = models.CharField(max_length=200, db_index=True)
+
     def __str__(self):
         return self.name
+
+
 class Task(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
     date = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
     category = models.ForeignKey('Categories', on_delete=models.CASCADE)
-    filter = models.ForeignKey('Filter', on_delete=models.CASCADE, null = True)
+    filter = models.ForeignKey('Filter', on_delete=models.CASCADE, null=True)
     hashtag = models.ManyToManyField(Hashtag)
-    notice = models.ForeignKey('Notice', on_delete=models.CASCADE, null = True)
+    notice = models.ForeignKey('Notice', on_delete=models.CASCADE, null=True)
     history = HistoricalRecords()
+
     def __str__(self):
         return "{0} — {1}".format(self.title, self.date)
-class Meta:
-        order_with_respect_to = 'user'
 
-    
+
+class Meta:
+    order_with_respect_to = 'user'
+
+
 class Filter(models.Model):
     name = models.CharField(max_length=200, db_index=True)
+
     def __str__(self):
         return self.name
+
+
 class Notice(models.Model):
-    notice= models.CharField(max_length=200)
+    notice = models.CharField(max_length=200)
+
     def __str__(self):
         return self.notice
